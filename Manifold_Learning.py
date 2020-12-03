af22b6b9ec813bcf76606ae1ab75e405bf3141c2
seanco <shon.cohen@cs.huji.ac.il>
2020-12-03 21:37:02 +0200

    first part

---
+++ Manifold_Learning.py
@@ -0,0 +1,155 @@
+import pickle
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets
+from mpl_toolkits.mplot3d import Axes3D
+
+
+def digits_example():
+    '''
+    Example code to show you how to load the MNIST data and plot it.
+    '''
+
+    # load the MNIST data:
+    digits = datasets.load_digits()
+    data = digits.data / 255.
+    labels = digits.target
+
+    # plot examples:
+    plt.gray()
+    for i in range(10):
+        plt.subplot(2, 5, i+1)
+        plt.axis('off')
+        plt.imshow(np.reshape(data[i, :], (8, 8)))
+        plt.title("Digit " + str(labels[i]))
+    plt.show()
+
+def swiss_roll_example():
+    '''
+    Example code to show you how to load the swiss roll data and plot it.
+    '''
+
+    # load the dataset:
+    X, color = datasets.samples_generator.make_swiss_roll(n_samples=2000)
+
+    # plot the data:
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=color, cmap=plt.cm.Spectral)
+    plt.show()
+
+
+def faces_example(path):
+    '''
+    Example code to show you how to load the faces data.
+    '''
+
+    with open(path, 'rb') as f:
+        X = pickle.load(f)
+
+    num_images, num_pixels = np.shape(X)
+    d = int(num_pixels**0.5)
+    print("The number of images in the data set is " + str(num_images))
+    print("The image size is " + str(d) + " by " + str(d))
+
+    # plot some examples of faces:
+    plt.gray()
+    for i in range(4):
+        plt.subplot(2, 2, i+1)
+        plt.imshow(np.reshape(X[i, :], (d, d)))
+    plt.show()
+
+
+def plot_with_images(X, images, title, image_num=25):
+    '''
+    A plot function for viewing images in their embedded locations. The
+    function receives the embedding (X) and the original images (images) and
+    plots the images along with the embeddings.
+
+    :param X: Nxd embedding matrix (after dimensionality reduction).
+    :param images: NxD original data matrix of images.
+    :param title: The title of the plot.
+    :param num_to_plot: Number of images to plot along with the scatter plot.
+    :return: the figure object.
+    '''
+
+    n, pixels = np.shape(images)
+    img_size = int(pixels**0.5)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.set_title(title)
+
+    # get the size of the embedded images for plotting:
+    x_size = (max(X[:, 0]) - min(X[:, 0])) * 0.08
+    y_size = (max(X[:, 1]) - min(X[:, 1])) * 0.08
+
+    # draw random images and plot them in their relevant place:
+    for i in range(image_num):
+        img_num = np.random.choice(n)
+        x0, y0 = X[img_num, 0] - x_size / 2., X[img_num, 1] - y_size / 2.
+        x1, y1 = X[img_num, 0] + x_size / 2., X[img_num, 1] + y_size / 2.
+        img = images[img_num, :].reshape(img_size, img_size)
+        ax.imshow(img, aspect='auto', cmap=plt.cm.gray, zorder=100000,
+                  extent=(x0, x1, y0, y1))
+
+    # draw the scatter plot of the embedded data points:
+    ax.scatter(X[:, 0], X[:, 1], marker='.', alpha=0.7)
+
+    return fig
+
+
+def MDS(X, d):
+    '''
+    Given a NxN pairwise distance matrix and the number of desired dimensions,
+    return the dimensionally reduced data points matrix after using MDS.
+
+    :param X: NxN distance matrix.
+    :param d: the dimension.
+    :return: Nxd reduced data point matrix.
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    pass
+
+
+def LLE(X, d, k):
+    '''
+    Given a NxD data matrix, return the dimensionally reduced data matrix after
+    using the LLE algorithm.
+
+    :param X: NxD data matrix.
+    :param d: the dimension.
+    :param k: the number of neighbors for the weight extraction.
+    :return: Nxd reduced data matrix.
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    pass
+
+
+def DiffusionMap(X, d, sigma, t):
+    '''
+    Given a NxD data matrix, return the dimensionally reduced data matrix after
+    using the Diffusion Map algorithm. The k parameter allows restricting the
+    kernel matrix to only the k nearest neighbor of each data point.
+
+    :param X: NxD data matrix.
+    :param d: the dimension.
+    :param sigma: the sigma of the gaussian for the kernel matrix transformation.
+    :param t: the scale of the diffusion (amount of time steps).
+    :return: Nxd reduced data matrix.
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    pass
+
+
+if __name__ == '__main__':
+
+    # TODO: YOUR CODE HERE
+
+    pass
